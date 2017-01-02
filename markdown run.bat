ffc0d81f091cc37eb6bc444e2c1ecba89f42594b
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-02 22:27:15 +0530

    add run file

--- markdown run.bat
+++ markdown run.bat
@@ -4,3 +4,11 @@ REM Creation Date : 2017-01-02
 REM Author : Lal Thomas 	
 REM Original File : D:\Dropbox\project\20151117-markdown todotxt cli addon\markdown 	
 	
+%~d0
+cd "%~p0"
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+pushd "%userProfile%\.todo.actions.d"
+echo "%scriptFolderPath%"
+mklink /d markdown "%scriptFolderPath%"
+popd
