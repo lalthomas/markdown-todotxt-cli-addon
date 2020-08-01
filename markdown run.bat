36984e370bd1649c49159d15fdfd27cfab8994c3
Îalthomas <lal.thomas.mail@gmail.com>
2020-08-01 16:55:33 +0700

    Update markdown run.bat

--- markdown run.bat
+++ markdown run.bat
@@ -1,8 +1,7 @@
 @echo OFF 	
 REM File : markdown run.bat 	
 REM Creation Date : 2017-01-02 	
-REM Author : Lal Thomas 	
-REM Original File : D:\Dropbox\project\20151117-markdown todotxt cli addon\markdown 	
+REM Author : Lal Thomas
 	
 %~d0
 cd "%~p0"
